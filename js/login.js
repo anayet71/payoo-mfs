7ce5710919e877fdbfde61bb39260ec8dbfd36b9
anayet71 <ariyananayet@gmail.com>
2024-12-03 08:05:25 +0600

    home page added

--- js/login.js
+++ js/login.js
@@ -3,9 +3,23 @@
 // step 1: set event handler
 document.getElementById("button-login").addEventListener('click', function(event){
     // step 2: prevent default behavior
-    event.preventDefault /*  to stop reload the page when btn clicked */
-    console.log("login button clicked")
-    // get phone number
+    event.preventDefault() /*  to stop reload the page when btn clicked */
+
+
+    // step 3: get phone number
     const phoneNumber = document.getElementById('phone-number').value
-    console.log(phoneNumber)
+    const pinNumber = document.getElementById("pin-number").value
+    console.log(phoneNumber , pinNumber)
+
+    // step 4: Verify the phone and pin
+    if(phoneNumber === '5' && pinNumber === "1234"){
+        console.log('You are logged in')
+        // step 5: allow user to use
+        phoneNumber.value = " ";
+        pinNumber.value = " "
+        window.location.href = "home.html";
+    }
+    else{
+        alert('Wrong phone number or pin')
+    }
 })
